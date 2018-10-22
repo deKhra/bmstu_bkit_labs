a4cdd63368f203fd02af5afed9c55895616ea95e
deKhra <hram1701@gmail.com>
2018-10-22 12:15:52 +0300

    no message

---
+++ lab4/lab4/lab4/ReadLines.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class ReadLines
+    {
+        Console.
+ 
+        string tempStrTrim = ""; 
+        //Список строк 
+        List<string> list = new List<string>(); 
+        do {     
+            //Временная переменная для хранения строки     
+            string tempStr = Console.ReadLine();     
+            //Удаление пробелов из введенной строки     
+            tempStrTrim = tempStr.Trim();     
+            if (tempStrTrim != ""){         
+                //Непустая строка сохраняется в список         
+                list.Add(tempStrTrim);     
+            } 
+        } while (tempStrTrim != ""); 
+    }
+}
